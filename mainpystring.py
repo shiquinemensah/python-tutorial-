97c837502f805d0dd14f8cb63dd2b562d2a50284
shiquine <godchildshiquine@gmail.com>
2021-11-03 08:02:07 -0700

    tutorial

---
+++ mainpystring.py
@@ -0,0 +1,12 @@
+#Working with Strings
+#how to use strings 
+phrase = 'Giraffe Academy'
+print (phrase.lower()) #how to check for lower case letters
+print (phrase.upper()) #how to check for upper case(capital letters)
+print(prase.isupper()) #allows the terminal to check and will give a true and false value
+print (phrase.upper.isupper())
+
+print(len(phrase)) #tells you how many characters there are within a string#
+print(phrase[0]) #in python indexes within a string will always start with 0 e.g 0123 what number I choose determines the character chosen
+int (phrase.index('G')) #counts the charcters for you
+print(phrase.replace('hello world', 'bye  plane'))#this allows you to replace the words through the terminal
